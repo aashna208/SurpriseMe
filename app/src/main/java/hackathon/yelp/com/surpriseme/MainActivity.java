baff3ea2398ae656a87311bf34ea37dd0dd20ec2
aashnam <aashnam@microsoft.com>
2016-03-06 19:26:24 -0800

    MainActivity and scribe lib

---
+++ app/src/main/java/hackathon/yelp/com/surpriseme/MainActivity.java
@@ -0,0 +1,30 @@
+package hackathon.yelp.com.surpriseme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import hackathon.yelp.com.surpriseme.datamodels.business.BusinessResult;
+import hackathon.yelp.com.surpriseme.networkrequest.AsyncListener;
+import hackathon.yelp.com.surpriseme.yelpinteraction.BusinessInteraction;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    BusinessInteraction businessInteraction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        businessInteraction = new BusinessInteraction();
+        final TextView textView = (TextView) findViewById(R.id.text_view);
+        businessInteraction.getBestMatchedResults("food", null, 20, new AsyncListener<BusinessResult>() {
+            @Override
+            public void OnResponseReceived(BusinessResult response) {
+                textView.setText(response == null ? String.valueOf(0) : String.valueOf(response.numberOfResults));
+
+            }
+        });
+    }
+}
